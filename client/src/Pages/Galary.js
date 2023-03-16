8166721a0c60a04176ae329c89a991e3f6ec6281
Programmer5768964 <svishwakarma6389@gmail.com>
2023-03-16 20:39:12 +0530

    logo in sidebar

---
+++ client/src/Pages/Galary.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Galary = () => {
+  return <div>This is Images/ videos Galary</div>;
+};
+
+export default Galary;
---
+++ client/src/Pages/OurFacility.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OurFacility = () => {
+  return <div>Thease are Our Facilities</div>;
+};
+
+export default OurFacility;
