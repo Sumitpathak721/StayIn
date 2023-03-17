fb3164aafacf92892d47fc8ca8f2eb102ee0e505
Sumit Pathak <pathaksumit721@gmail.com>
2023-03-18 04:20:54 +0530

    Add Encryption to password

--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,8 @@ import Auth from "./Pages/Auth/Auth.js";
 import Sidebar from "./Components/SideBar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+var auth = localStorage.getItem('user');
+auth  = JSON.parse(auth);
 root.render(
   <div>
     <BrowserRouter>
@@ -17,7 +19,26 @@ root.render(
           <Route path="/" element={<Home />} />
           <Route path="/aboutus" element={<AboutUs />} />
           <Route path="/contactus" element={<ContactUs />} />
+          {(auth)? 
+            <>
+              {
+              (auth.Access==="Organization")?
+                <Route path="/viewHostel" element={<h1>View Hostel</h1>}/>:
+              <>
+                {(auth.Access==="Developer")?
+                <Route path="/DB" element={<h1>DB toh hona chaiye</h1>} />
+                :<></>
+                }
+              </>
+              }
+              <Route path="/profile" element={<h1>Profile</h1>}/>
+            </>
+
+            :<></>
+          }
+          
           <Route path="/auth" element={<Auth />} />
+          <Route path="/*" element={<h1>404 Page not Found</h1>}/>
         </Routes>
       </Sidebar>
     </BrowserRouter>
